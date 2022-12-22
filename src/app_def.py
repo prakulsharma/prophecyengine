d760131e8fcd9d3480c479888f80aa75c6d26482
Prakul Sharma <prakul.1307@gmail.com>
2022-12-22 13:45:00 +0530

    Delete app_def.py

--- src/app_def.py
+++
@@ -1,6 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-# Using SPACELAB theme from dash bootstrap components
-app = dash.Dash(external_stylesheets=[dbc.themes.SPACELAB],  suppress_callback_exceptions=True)
-server = app.server
