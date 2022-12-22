fbfcec4aefc7a75ba2e85c7060a92efb48aa350e
Prakul Sharma <prakul.1307@gmail.com>
2022-12-22 13:42:41 +0530

    delete

--- src/model-code/src/methods/AUTOENCODER/__init__.py
+++
@@ -1,1 +0,0 @@
-from .AUTOENCODER import AUTOENCODER
--- src/model-code/src/methods/__init__.py
+++
@@ -1,6 +0,0 @@
-from .OCSVM import OCSVM
-from .PCA import PCA
-from .KNN import KNN
-from .AUTOENCODER import AUTOENCODER
-
-available_methods = [OCSVM, PCA, KNN, AUTOENCODER]
